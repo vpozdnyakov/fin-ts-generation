c181e25c1dbb68a2fd628e786154e5c1b51b8bc4
ramil.chermanteev <ramil.chermanteev@sbermarket.ru>
2023-09-12 09:30:05 +0300

    add model tcn vae

--- fints_generation/models/__init__.py
+++ fints_generation/models/__init__.py
@@ -1,7 +1,9 @@
 from fints_generation.models.lstm_vae import LSTMVAE
 from fints_generation.models.tcn_gan import TCNGAN
+from fints_generation.models.tcn_vae import TCNVAE
 
 __all__ = [
     'LSTMVAE',
-    'TCNGAN'
+    'TCNGAN',
+    'TCNVAE'
 ]
